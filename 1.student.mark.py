d7a2ae4a3d6983d85205ed99fafa0aa89754e97a
phuongthunee <thulp.bi11-258@st.usth.edu.vn>
2022-03-23 05:09:01 +0700

    Updated

--- 1.student.mark.py
+++ 1.student.mark.py
@@ -1,10 +1,11 @@
 print ("STUDENT MARK MANAGEMENT")
 print ("")
-#-----------------------------------------
+
+# Input number of students in a class
 def numOfStudents ():
     return int(input("How many students are there in this class?: "))
-
 #-----------------------------------------
+# Input student information: id, name, DoB
 print ("STUDENT INFORMATION")
 def getStudentInfor ():
     Student_ID = input ("ID: ")
@@ -25,17 +26,21 @@ print ("No.\t ID\t Name\t DoB\t ")
 for i in range (0, len(listOfStudents), 1):
     s = listOfStudents[i]
     print ((i+1), "\t", s["Student_ID: "], "\t", s["Student_Name: "], "\t", s["Student_DoB: "])
+# List students
 #-----------------------------------------
+
+#Input number of courses
 def numOfCourses ():
     return int(input ("How many courses are there in this class?: "))
-
 #-----------------------------------------
+# Input course information: id, name
 print ("")
 print ("COURSE INFORMATION")
 def getCourseInfor ():
     Course_ID = input ("ID: ")
     Course_Name = input ("Name: ")
     return Course_ID, Course_Name
+
 inforC = numOfCourses ()
 listOfCourses = []
 for i in range (inforC):
@@ -48,31 +53,32 @@ print ("No.\t ID\t Name\t ")
 for i in range (0, len(listOfCourses), 1):
     s = listOfCourses[i]
     print ((i+1), "\t", s["ID"], "\t", s["Name"])
-    
+#List courses
 #-----------------------------------------
-#-----------------------------------------
-def numOfMarks ():
-    return int(input ("How many marks do you want to input?: "))
 
-#-----------------------------------------
+# Select a course, input marks for student in this course
 print ("")
 print ("SELECT A COURSE, INPUT MARKS FOR STUDENTS IN THIS COURSE")
 
-def getMarkInfor ():
-    Course_ID = input ("Course ID: ")
-    Student_ID = input ("Student ID: ")
-    Mark = input ("Mark: ")
-    return Course_ID, Student_ID, Mark
-inforM = numOfMarks ()
-marks = []
-for i in range (inforM):
-    Course_ID, Student_ID, Mark = getMarkInfor ()
-    print ("") 
-    inforM = {"Course ID": Course_ID,
-              "Student ID": Student_ID,
-              "Mark": Mark}
-    marks.append (inforM)
-print ("No.\t ID\t Name\t ")
-for i in range (0, len(marks), 1):
-    s = marks [i]
-    print ((i+1), "\t", s["Course ID"], "\t", s["Student ID"], "\t", s["Mark"])
+StudentMark = []
+selectCourse = 0
+while selectCourse in range (0, len(listOfCourses), 1):
+    selectCourse = int(input("Select a course: "))
+    if selectCourse > len(listOfCourses):
+        break
+    for i in range (0, len(listOfStudents), 1):
+        print (f"Course {selectCourse} - input mark for student {Student_ID[0]}: ")
+        mark = int(input())
+        StudentMark.append({Student_ID[0]: mark})
+        
+#-----------------------------------------
+# Show student marks for a given course
+selectCourse = input("Which course do you want to see all student marks?: ")
+if selectCourse in StudentMark:
+    print (f"Course {selectCourse} mark: ")    
+    for i in range (len(numOfStudents)):
+        print (f"Student {Student_ID[i]}: {StudentMark[selectCourse][i]}")
+else:
+    print ("Wrong! Input again...")    
+    
+print ("Done!")
